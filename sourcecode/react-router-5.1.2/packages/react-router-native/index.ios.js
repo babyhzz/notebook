fc5dbe808c9f53caf1d21a38ed95ff236b20c6ed
hc <383686357@qq.com>
2020-03-04 12:02:17 +0800

    react router sourcecode

---
+++ sourcecode/react-router-5.1.2/packages/react-router-native/index.ios.js
@@ -0,0 +1,8 @@
+import { AppRegistry } from "react-native";
+
+import ReactRouterNative from "./examples/BasicExample";
+//import ReactRouterNative from './examples/ExperimentalExample'
+
+AppRegistry.registerComponent("ReactRouterNative", () => ReactRouterNative);
+
+export default ReactRouterNative;
