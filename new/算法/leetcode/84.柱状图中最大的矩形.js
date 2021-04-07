daf20130a49aa77779904f501a0784fb98a39cef
hucheng <383686357@qq.com>
2021-04-07 16:47:06 +0800

    update

--- new/算法/leetcode/84.柱状图中最大的矩形.js
+++ new/算法/leetcode/84.柱状图中最大的矩形.js
@@ -9,40 +9,77 @@
  * @param {number[]} heights
  * @return {number}
  */
-var largestRectangleArea = function (heights) {
-  var stack = [];
-  var maxArea = 0;
 
-  for (let i = 0; i < heights.length; i++) {
-    if (stack.length === 0) {
-      stack.push(heights[i]);
-    } else {
-      let top = stack[stack.length - 1];
-      if (heights[i] >= top) {
-        stack.push(heights[i]);
-      } else {
-        let back = 1;
-        while (heights[i] < top && top) {
-          maxArea = Math.max(maxArea, stack.pop() * back);
-          top = stack[stack.length - 1];
-          back++;
-        }
-      }
-    }
+class Stack {
+  constructor() {
+    this.arr = [];
   }
 
-  if (stack.length !== 0) {
-    let top = stack[stack.length - 1];
-    let back = 1;
-    while (top) {
-      maxArea = Math.max(maxArea, stack.pop() * back);
-      top = stack[stack.length - 1];
-      back++;
+  isEmpty() {
+    return this.arr.length === 0;
+  }
+
+  peek() {
+    return this.arr[this.arr.length - 1];
+  }
+
+  pop() {
+    return this.arr.pop();
+  }
+
+  push(v) {
+    this.arr.push(v);
+  }
+}
+
+// 不填充0的版本
+var largestRectangleArea1 = function (heights) {
+  const n = heights.length;
+  const stack = new Stack();
+  let maxArea = 0;
+
+  for (let i = 0; i < n; i++) {
+    while (!stack.isEmpty() && heights[i] < heights[stack.peek()]) {
+      const h = heights[stack.pop()];
+      const w = stack.isEmpty() ? i : i - stack.peek() - 1;
+      maxArea = Math.max(maxArea, w * h);
     }
+
+    stack.push(i);
+  }
+
+  while (stack.length !== 0) {
+    const h = heights[stack.pop()];
+    const w = stack.length === 0 ? n : n - stack[stack.length - 1] - 1;
+    maxArea = Math.max(maxArea, w * h);
   }
 
   return maxArea;
 };
+
+// 填充0的版本，不用那么多的临界判断
+var largestRectangleArea = function (heights) {
+  const stack = new Stack();
+  stack.push(0);
+  heights.push(0);
+  heights.unshift(0);
+
+  const n = heights.length;
+  let maxArea = 0;
+
+  for (let i = 1; i < n; i++) {
+    while (heights[i] < heights[stack.peek()]) {
+      const h = heights[stack.pop()];
+      const w = i - stack.peek() - 1;
+      maxArea = Math.max(maxArea, w * h);
+    }
+
+    stack.push(i);
+  }
+
+  return maxArea;
+};
+
 // @lc code=end
 
-console.log(largestRectangleArea([1, 1]));
+console.log(largestRectangleArea([4, 2, 0, 3, 2, 5]));
---
+++ new/算法/leetcode/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+  // for (let i = 0; i < n; i++) {
+  //   while (!isEmpty(stack) && heights[i] < peek(heights, stack)) {
+  //     // 开始计算
+  //   }
+
+  //   stack.push(i);
+  // }
